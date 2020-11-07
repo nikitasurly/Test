367aae1113851c65e44341dd0bd567b00ee7e680
nikita <mikropyl28@gmail.com>
2020-11-08 02:01:35 +0300

    ???

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Alex", "Brown", (byte) 33);
+        userService.saveUser("Anna", "White", (byte) 18);
+        userService.saveUser("Nick", "Cox", (byte) 24);
+        userService.saveUser("Oleg", "Ivanov", (byte) 24);
+        userService.getAllUsers().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,14 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/testDB?serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection connect() throws SQLException {
+        Connection con;
+        return con = DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
